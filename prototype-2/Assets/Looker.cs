45a472cd22a28685d70bf51616bd8f8f3b1d0fba
jessikaforster <jessikaforster@icloud.com>
2023-02-21 13:46:28 -0500

    Start prototype 2

---
+++ prototype-2/Assets/Looker.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Looker : MonoBehaviour
+{
+
+    public GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        this.GetComponent<Transform>().LookAt(target.transform);
+
+        this.transform.Translate(new Vector3(0,0,0.005f), Space.Self);
+    }
+}
